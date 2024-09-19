850633e16e711236d5a132235cae8203ee1e4b0d
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-19 13:47:52 +0300

    chore: init driver microservice

---
+++ src/main/java/com/software/modsen/drivermicroservice/DriverMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.software.modsen.drivermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverMicroserviceApplication.class, args);
+    }
+
+}
