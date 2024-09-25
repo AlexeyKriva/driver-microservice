8bd13c7c95eaf331d128e090560634076b554d2a
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-25 17:38:00 +0300

    chore: inin eureka client

--- src/main/java/com/software/modsen/drivermicroservice/DriverMicroserviceApplication.java
+++ src/main/java/com/software/modsen/drivermicroservice/DriverMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.software.modsen.drivermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableDiscoveryClient
 public class DriverMicroserviceApplication {
 
     public static void main(String[] args) {
