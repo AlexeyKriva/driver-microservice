2a97e11beb9e2cc362e222186a935c0c7d944af6
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-30 17:27:43 +0300

    fix: update data source

--- src/main/java/com/software/modsen/drivermicroservice/configs/DataSourceConfig.java
+++ src/main/java/com/software/modsen/drivermicroservice/configs/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.software.modsen.drivermicroservice.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -8,13 +9,25 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/cab-aggregator-db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("98479847");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         return dataSource;
     }
--- src/main/java/com/software/modsen/drivermicroservice/controllers/DriverAccountController.java
+++ src/main/java/com/software/modsen/drivermicroservice/controllers/DriverAccountController.java
@@ -8,6 +8,7 @@ import com.software.modsen.drivermicroservice.services.DriverAccountService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -41,6 +42,7 @@ public class DriverAccountController {
     }
 
     @PutMapping("/{driver_id}/increase")
+    @Transactional
     public ResponseEntity<DriverAccount> increaseBalanceByDriverId(
             @PathVariable("driver_id") long driverId,
             @Valid @RequestBody DriverAccountIncreaseDto driverAccountIncreaseDto) {
